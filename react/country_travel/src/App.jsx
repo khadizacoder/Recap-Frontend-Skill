3d76c3e1bb35b6dff50d8e248e8ec48315edb01a
khadizacoder <khadiza.dev@gmail.com>
2026-08-01 10:08:20 -0700

    recap react

---
+++ react/country_travel/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Hero from './components/Hero'
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <h2>Hello, Vite + React!</h2>
+    </>
+  )
+}
+
+export default App
---
+++ react/country_travel/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import heroImg from '/favicon.png'
+
+export default function Hero() {
+  return (
+    <div>
+        <div>
+            <div className="logo">
+                <img src={heroImg} alt="Logo" />
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
