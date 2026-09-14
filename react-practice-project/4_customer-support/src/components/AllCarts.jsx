48389f5161616f963c33501715e89420602b1e98
khadizacoder <khadiza.dev@gmail.com>
2026-09-14 11:58:25 +0600

    Final Update

--- react-practice-project/4_customer-support/src/components/AllCarts.jsx
+++ react-practice-project/4_customer-support/src/components/AllCarts.jsx
@@ -5,7 +5,7 @@ export default function AllCarts({custommerData, handleProgress}) {
     const allData = use(custommerData);
 
   return (
-    <div className='mt-2 grid grid-cols-1 md:grid-cols-2 gap-6 mr-10'>
+    <div className='mt-2 grid grid-cols-1 lg:grid-cols-2 gap-6 md:mr-10'>
       {
         allData.map(cart => <Cart key={cart.id} cart={cart} handleProgress={handleProgress}/> )
       }
